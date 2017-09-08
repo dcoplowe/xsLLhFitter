4717b50b6e3a32ffc5cfc344f142e52cd6e6dcfb
dcoplowe <david.coplowe@physics.ox.ac.uk>
2017-09-08 16:08:45 -0500

    Correction to makefile

--- minerva/app/app_detsysts.cpp
+++ minerva/app/app_detsysts.cpp
@@ -9,7 +9,7 @@ int main(int argc, char const *argv[])
 	std::string out_file = "DetSyst_Test.root";
 	bool verbose = true;
 
-	DetectorSystematics syst(in_file, in_tree, out_file verbose);
+	DetectorSystematics syst(in_file, in_tree, out_file, verbose);
 	syst.Run();
 
 	return 1;
