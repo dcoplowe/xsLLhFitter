895c68a32837e1ab432be9fece4f9f4ddfb53177
dcoplowe <david.coplowe@physics.ox.ac.uk>
2017-09-11 17:05:52 -0500

    Debuging base classes and fileio

--- minerva/app/app_detsysts.cpp
+++ minerva/app/app_detsysts.cpp
@@ -17,9 +17,21 @@ int main()
 
 	DetectorSystematics syst(10, verbose);
 
-	syst.AddSample("signal", 29, 0., 20000.);
-	syst.AddSample("signal", 29, -300., 300.);
-	// syst.AddSample("Wgt1800", 29, 1800., 10000.);
+	syst.AddSample("signal",  29, 0., 20000.);
+	syst.AddSample("signal",  29, -300., 300.);
+	double dpv = new [ 6 ];
+	dpv[0] = 1800.;
+	dpv[1] = 2200.;
+	dpv[2] = 3000.;
+	dpv[3] = 5000.;
+	dpv[4] = 6000.;
+	dpv[5] = 10000.;
+
+	syst.AddSample("Wgt1800", 5, dpv);
+
+	double ffffd[ 6 ] = { 1800., 2200., 3000., 5000., 6000., 10000. };
+	syst.AddSample("Wgt1800", ffffd);
+
 
 	FileIO reader(in_file, in_tree);
 
@@ -29,6 +41,7 @@ int main()
 	for(Int_t i = 0; i < 10; i++){
 		reader.GetEntry(i);
 		cout << "reader.muon_E = " << reader.muon_E << endl;
+
 		syst.FillSample("signal", reader.muon_E, reader.mc_cvweight_total );
 	}
 
