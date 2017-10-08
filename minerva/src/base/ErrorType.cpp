96f82fb2aef464a03a86050da525567e677bb544
dcoplowe <david.coplowe@physics.ox.ac.uk>
2017-10-08 14:03:24 -0500

    Written first outline for model uncertainty parameters

--- minerva/src/base/ErrorType.cpp
+++ minerva/src/base/ErrorType.cpp
@@ -5,7 +5,7 @@
 
 ErrorType::ErrorType(const std::string &name, const int n_universes, const Type type, const std::string &fill_samples) : m_name(name), m_Nuniverses(n_universes), m_type(type)
 {
-	m_fill_sample.push_back(fill_samples);
+	m_fill_samples.push_back(fill_samples);
 }
 
 ErrorType::~ErrorType()
