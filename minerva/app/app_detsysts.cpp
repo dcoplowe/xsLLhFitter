dffd46ad0eb662069eb261643c8303fac0ab9373
dcoplowe <david.coplowe@physics.ox.ac.uk>
2017-10-09 15:34:23 -0500

    Debuging the minerva framework to ll fitter

--- minerva/app/app_detsysts.cpp
+++ minerva/app/app_detsysts.cpp
@@ -96,6 +96,7 @@ int main()
 
 	// In order to produce a covariance matrix need to vary ALL systs in ALL samples
 	// 1) we want to add a variation to all samples 
+	cout << "Make Covariance Matrix" << endl;	
 	TMatrixD cov = syst.GetCovMatrix();
 	cov.Print();
 	// cov.Write("detsyst");
