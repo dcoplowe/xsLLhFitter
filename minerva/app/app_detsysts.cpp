4e645483d92e177739bc322445b79aa05dae73b1
dcoplowe <david.coplowe@physics.ox.ac.uk>
2017-10-09 15:07:56 -0500

    Debuging the minerva framework to ll fitter

--- minerva/app/app_detsysts.cpp
+++ minerva/app/app_detsysts.cpp
@@ -68,19 +68,19 @@ int main()
 		// Want to make sure only one sample is filled in each interation
 		if(0. < reader.pi0_invMass && reader.pi0_invMass <= lowMass){
 			// cout << "Filling Sample pi0LowMass" << endl;
-			syst.FillSample("pi0LowMass", reader.pi0_invMass, reader.wgt);
+			// syst.FillSample("pi0LowMass", reader.pi0_invMass, reader.wgt);
 			// syst.FillVertErrorBand("pi0LowMass", "Flux_BeamFocus", reader.pi0_invMass, reader.mc_wgt_Flux_BeamFocus, reader.wgt);
 			// syst.FillVertErrorBand("pi0LowMass", "ppfx1_Total", reader.pi0_invMass, reader.mc_wgt_ppfx1_Total, reader.wgt);
 			// reader.SetSample(0);
 		}
 		else if(lowMass < reader.pi0_invMass && reader.pi0_invMass < higMass){
-			syst.FillSample("signal", reader.pi0_invMass, reader.wgt);
+			// syst.FillSample("signal", reader.pi0_invMass, reader.wgt);
 			// syst.FillVertErrorBand("signal", "Flux_BeamFocus", reader.pi0_invMass, reader.mc_wgt_Flux_BeamFocus, reader.wgt);
 			// syst.FillVertErrorBand("signal", "ppfx1_Total", reader.pi0_invMass, reader.mc_wgt_ppfx1_Total, reader.wgt);
 			// reader.SetSample(1);
 		}
 		else if(higMass <= reader.pi0_invMass && reader.pi0_invMass < maxMass){
-			syst.FillSample("pi0HigMass", reader.pi0_invMass, reader.wgt);
+			// syst.FillSample("pi0HigMass", reader.pi0_invMass, reader.wgt);
 			// syst.FillVertErrorBand("pi0HigMass", "Flux_BeamFocus", reader.pi0_invMass, reader.mc_wgt_Flux_BeamFocus, reader.wgt);
 			// syst.FillVertErrorBand("pi0HigMass", "ppfx1_Total", reader.pi0_invMass, reader.mc_wgt_ppfx1_Total, reader.wgt);
 			// reader.SetSample(2);
@@ -98,9 +98,9 @@ int main()
 
 	// In order to produce a covariance matrix need to vary ALL systs in ALL samples
 	// 1) we want to add a variation to all samples 
-	TMatrixD cov = syst.GetCovMatrix();
+	// TMatrixD cov = syst.GetCovMatrix();
 	// cov.Print();
-	cov.Write("detsyst");
+	// cov.Write("detsyst");
 
 	ofile->Close();
 	delete ofile;
