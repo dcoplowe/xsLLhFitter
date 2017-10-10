167eccf7b568891cec61602b3f1d32efa11125e1
dcoplowe <david.coplowe@physics.ox.ac.uk>
2017-10-09 23:00:16 -0500

    Debuging the minerva framework to ll fitter

--- minerva/app/app_detsysts.cpp
+++ minerva/app/app_detsysts.cpp
@@ -22,7 +22,7 @@ int main()
 	// WTf do I mean here??? IDIOT
 	// Ah the number of toys is the systname_sz
 
-	DetectorSystematics * syst = new DetectorSystematics(100, verbose);
+	// DetectorSystematics * syst = new DetectorSystematics(100, verbose);
 
 	// Ozgur's signal:
 	// In ten's of MeV
@@ -34,9 +34,9 @@ int main()
 	int nsigMass_bins = (int)(higMass - lowMass)/10.;
 	int nhigMass_bins = (int)(maxMass - higMass)/10.;
 
-	syst->AddSample("pi0LowMass", nlowMass_bins, 0.,      lowMass);
-	syst->AddSample("signal",  	 nsigMass_bins, lowMass, higMass);
-	syst->AddSample("pi0HigMass", nhigMass_bins, higMass, maxMass);
+	// syst->AddSample("pi0LowMass", nlowMass_bins, 0.,      lowMass);
+	// syst->AddSample("signal",  	 nsigMass_bins, lowMass, higMass);
+	// syst->AddSample("pi0HigMass", nhigMass_bins, higMass, maxMass);
 	
 	// Both his sidebands:
 	// syst->AddSample("sideband",  100, 10000., 20000.);
@@ -45,10 +45,10 @@ int main()
 
 	TFile * ofile = FileIO::MakeOutFile(out_file);
 	ofile->cd();
-	// // reader.SetupLLNTuple();
+	reader.SetupLLNTuple();
 
-	syst->AddVertErrorBand("Flux_BeamFocus", reader.mc_wgt_Flux_BeamFocus_sz);
-	syst->AddVertErrorBand("ppfx1_Total", reader.mc_wgt_ppfx1_Total_sz);
+	// syst->AddVertErrorBand("Flux_BeamFocus", reader.mc_wgt_Flux_BeamFocus_sz);
+	// syst->AddVertErrorBand("ppfx1_Total", reader.mc_wgt_ppfx1_Total_sz);
 
 	cout << "reader.GetEntries() = " << reader.GetEntries() << endl;
 
@@ -69,39 +69,39 @@ int main()
 			syst->FillSample("pi0LowMass", reader.pi0_invMass, reader.wgt);
 			syst->FillVertErrorBand("pi0LowMass", "Flux_BeamFocus", reader.pi0_invMass, reader.mc_wgt_Flux_BeamFocus, reader.wgt);
 			syst->FillVertErrorBand("pi0LowMass", "ppfx1_Total", reader.pi0_invMass, reader.mc_wgt_ppfx1_Total, reader.wgt);
-			// reader.SetSample(0);
+			reader.SetSample(0);
 		}
 		else if(lowMass < reader.pi0_invMass && reader.pi0_invMass < higMass){
 			syst->FillSample("signal", reader.pi0_invMass, reader.wgt);
 			syst->FillVertErrorBand("signal", "Flux_BeamFocus", reader.pi0_invMass, reader.mc_wgt_Flux_BeamFocus, reader.wgt);
 			syst->FillVertErrorBand("signal", "ppfx1_Total", reader.pi0_invMass, reader.mc_wgt_ppfx1_Total, reader.wgt);
-			// reader.SetSample(1);
+			reader.SetSample(1);
 		}
 		else if(higMass <= reader.pi0_invMass && reader.pi0_invMass < maxMass){
-			syst->FillSample("pi0HigMass", reader.pi0_invMass, reader.wgt);
-			syst->FillVertErrorBand("pi0HigMass", "Flux_BeamFocus", reader.pi0_invMass, reader.mc_wgt_Flux_BeamFocus, reader.wgt);
-			syst->FillVertErrorBand("pi0HigMass", "ppfx1_Total", reader.pi0_invMass, reader.mc_wgt_ppfx1_Total, reader.wgt);
-			// reader.SetSample(2);
+			// syst->FillSample("pi0HigMass", reader.pi0_invMass, reader.wgt);
+			// syst->FillVertErrorBand("pi0HigMass", "Flux_BeamFocus", reader.pi0_invMass, reader.mc_wgt_Flux_BeamFocus, reader.wgt);
+			// syst->FillVertErrorBand("pi0HigMass", "ppfx1_Total", reader.pi0_invMass, reader.mc_wgt_ppfx1_Total, reader.wgt);
+			reader.SetSample(2);
 		}
 		else{
 			cout << "Warning Bad Range: " << reader.pi0_invMass << endl;
-			// reader.SetSample(3);
+			reader.SetSample(3);
 		}
-		// cout << "Now filling tree." << endl;
-		// reader.Fill();
+		cout << "Now filling tree." << endl;
+		reader.Fill();
 	}
 
-	// reader.Write();
+	reader.Write();
 	// In order to produce a covariance matrix need to vary ALL systs in ALL samples
 	// 1) we want to add a variation to all samples 
 	cout << "Make Covariance Matrix" << endl;	
-	TMatrixD cov = syst->GetCovMatrix();
-	// Need to find seg fault in this class...
-	// Seems to be some memory problem... :(
-	cov.Print();
-	cov.Write("detsyst");
+	// TMatrixD cov = syst->GetCovMatrix();
+	// // Need to find seg fault in this class...
+	// // Seems to be some memory problem... :(
+	// cov.Print();
+	// cov.Write("detsyst");
 
-	delete syst;
+	// delete syst;
 
 	ofile->Close();
 	delete ofile;
