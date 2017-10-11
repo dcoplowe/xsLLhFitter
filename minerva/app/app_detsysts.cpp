1829f98a11da35fd8cf1ce6269a7fc46ab2a52ac
dcoplowe <david.coplowe@physics.ox.ac.uk>
2017-10-11 16:37:04 -0500

    Making our first covariance matrix

--- minerva/app/app_detsysts.cpp
+++ minerva/app/app_detsysts.cpp
@@ -100,7 +100,7 @@ int main()
 	// In order to produce a covariance matrix need to vary ALL systs in ALL samples
 	// 1) we want to add a variation to all samples 
 	cout << "Make Covariance Matrix" << endl;	
-	TMatrixD cov = syst->GetCovMatrix("A", false, false);
+	TMatrixD cov = syst->GetCovMatrix("A", false, true);
 	// // Need to find seg fault in this class...
 	// // Seems to be some memory problem... :(
 	// cov.Print();
