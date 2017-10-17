f114426a816942a9d2fd72894f5b5ebd6b589199
dcoplowe <david.coplowe@physics.ox.ac.uk>
2017-10-17 13:12:57 -0500

    Added vars for recalculation of muon direction

---
+++ minerva/scripts/MakeFileIO.sh
@@ -0,0 +1,35 @@
+source $(readlink -f ~/.profile)
+
+version="v10r8p12"
+minbase="/grid/fermiapp/minerva/software_releases/${version}/setup.sh"
+
+if [ -z ${ROOTSYS+x} ]; then 
+	echo "No root version. Sourcing from minerva version ${version}"
+	source ${minbase}
+fi
+
+
+input_error()
+{
+    echo "Run script as follow: ./MakeFileIO.sh root_file.root tree_name"
+}
+
+rootfile=$1
+treename=$2
+
+if [ -z ${rootfile+x} ]; then
+	echo "No root file defined"
+	input_error
+	return
+fi
+
+if [ -z ${treename+x} ]; then
+	echo "No tree defined"
+	input_error
+	return
+fi
+
+# This could be improved (using PyRoot):
+root -l -b ${rootfile} <<EOF
+${treename}->MakeClass();
+EOF
