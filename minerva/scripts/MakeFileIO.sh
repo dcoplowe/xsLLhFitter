b3a268c156a389646f34a520dc79ff8cd655bd9c
dcoplowe <david.coplowe@physics.ox.ac.uk>
2017-10-17 13:58:00 -0500

    Added vars for recalculation of muon direction

--- minerva/scripts/MakeFileIO.sh
+++ minerva/scripts/MakeFileIO.sh
@@ -50,5 +50,5 @@ replace_cpp="__ADD_PUBLIC_VARS_AND_BRANCH_INITIALISATION_HERE__"
 # replacewith_h=$(grep "fChain->SetBranchAddress" ${treename}.h)
 replacewith_cpp=$(grep "fChain->SetBranchAddress" ${treename}.h)
 
-sed "s/${replace_cpp}/${replacewith_cpp}/" FileIO.cpp
+sed "s/${replace_cpp}/${replacewith_cpp}/g" FileIO.cpp
 # first=// Declaration of leaf types
