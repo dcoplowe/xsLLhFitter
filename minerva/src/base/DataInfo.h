81852d03d2b0d78bbdc4cfff9c33d375d94a11e5
dcoplowe <david.coplowe@physics.ox.ac.uk>
2017-10-30 14:57:57 -0500

    Can now account for under/overflow bins and whether or not to include them when producing the covariance matrix

--- minerva/src/base/DataInfo.h
+++ minerva/src/base/DataInfo.h
@@ -1,6 +1,9 @@
 #ifndef __DATAINFO_H__
 #define __DATAINFO_H__
 
+#include <string>
+#include <ReadParam.h>
+
 namespace DataInfo{
 	namespace POT{
 		const double DATA = 3.33153e+20;
@@ -22,11 +25,24 @@ namespace DataInfo{
 		const double Pi0 = 134.9766;// MeV
 		const double Pi0Sq = Pi0*Pi0;
 
+		const double Proton = 938.2720813;// MeV
+		const double ProtonSq = Proton*Proton;
+
 		// const double Pion = 134.9766;// MeV
 		// const double PionSq = Pion*Pion;
 
 		const double Muon = 105.6583745;// MeV
 		const double MuonSq = Muon*Muon;
+	}
+
+	namespace Opts{
+		const std::string base_file = "options/options_files.txt";
+
+		const std::string Run = "options/" + ReadParam::GetParameterS("run", base_file);
+		const std::string ReconPars = "options/" + ReadParam::GetParameterS("recon", base_file);
+		const std::string ManagePlots = "options/" + ReadParam::GetParameterS("manage_plots", base_file);
+		const std::string TransPlots = "options/" + ReadParam::GetParameterS("transverse_plots", base_file);
+		const std::string PartiPlots = "options/" + ReadParam::GetParameterS("particle_plots", base_file);
 	}
 }
 
